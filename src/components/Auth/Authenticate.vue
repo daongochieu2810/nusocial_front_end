80bcce7dad2f146066dac2a570ea3dbd7b0bbc58
Adithya Narayan Rangarajan Sreenivasan <r.s.adithya.narayan@gmail.com>
2020-07-17 11:24:31 +0530

    finished luminus

--- src/components/Auth/Authenticate.vue
+++ src/components/Auth/Authenticate.vue
@@ -4,7 +4,7 @@
 
 <script>
 const params = window.location.search;
-
+const fb = require("../../backend.js");
 const code = new URLSearchParams(params).get("code");
 
 const url = "https://nusocial-bridge-api.herokuapp.com/auth?code=" + code;
@@ -39,7 +39,41 @@ request.post(
         },
         function(error, response, body) {
           if (!error) {
-            console.log(body);
+            const userID = body;
+            process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
+            request.post(
+              {
+                url:
+                  "https://nusocial-bridge-api.herokuapp.com/firebaseToken?id=" +
+                  userID,
+                headers: {
+                  "Content-Type": "application/x-www-form-urlencoded"
+                },
+                rejectUnauthorized: false,
+                requestCert: false,
+                agent: false
+              },
+              function(error, response, request) {
+                if (!error) {
+                  const firebaseToken = body;
+                  console.log(firebaseToken);
+                  fb.auth
+                    .signInWithCustomToken(firebaseToken)
+                    .then(user => {
+                      console.log(user.user);
+                      this.$store.commit("setCurrentUser", user.user);
+                      this.$store.dispatch("fetchUserProfile").then(() => {
+                        this.$router.push("/");
+                        this.$store.commit("toggleLoading");
+                      });
+                    })
+                    .catch(err => {
+                      console.log(err);
+                      this.$store.commit("toggleLoading");
+                    });
+                }
+              }
+            );
           }
         }
       );
