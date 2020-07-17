e8af11eee37fd81751e5ba791425071d9a5dfa0b
Adithya Narayan Rangarajan Sreenivasan <r.s.adithya.narayan@gmail.com>
2020-07-17 11:33:17 +0530

    finished luminus

--- src/components/Auth/Authenticate.vue
+++ src/components/Auth/Authenticate.vue
@@ -1,5 +1,5 @@
 <template>
-  <p>Hi, authenticating</p>
+    <p>Hi, authenticating</p>
 </template>
 
 <script>
@@ -11,74 +11,84 @@ const url = "https://nusocial-bridge-api.herokuapp.com/auth?code=" + code;
 process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
 var request = require("request");
 request.post(
-  {
-    url: url,
-    headers: {
-      "Content-Type": "application/x-www-form-urlencoded"
-    },
-    rejectUnauthorized: false,
-    requestCert: false,
-    agent: false
-  },
-  function(error, response, body) {
-    if (!error) {
-      var accessToken = JSON.parse(body)["access_token"];
-      console.log(accessToken);
-      process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
-      request.post(
-        {
-          url:
-            "https://nusocial-bridge-api.herokuapp.com/profile?token=" +
-            accessToken,
-          headers: {
-            "Content-Type": "application/x-www-form-urlencoded"
-          },
-          rejectUnauthorized: false,
-          requestCert: false,
-          agent: false
+    {
+        url: url,
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
         },
-        function(error, response, body) {
-          if (!error) {
-            const userID = body;
+        rejectUnauthorized: false,
+        requestCert: false,
+        agent: false,
+    },
+    function(error, response, body) {
+        if (!error) {
+            var accessToken = JSON.parse(body)["access_token"];
+            console.log(accessToken);
             process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
             request.post(
-              {
-                url:
-                  "https://nusocial-bridge-api.herokuapp.com/firebaseToken?id=" +
-                  userID,
-                headers: {
-                  "Content-Type": "application/x-www-form-urlencoded"
+                {
+                    url:
+                        "https://nusocial-bridge-api.herokuapp.com/profile?token=" +
+                        accessToken,
+                    headers: {
+                        "Content-Type": "application/x-www-form-urlencoded",
+                    },
+                    rejectUnauthorized: false,
+                    requestCert: false,
+                    agent: false,
                 },
-                rejectUnauthorized: false,
-                requestCert: false,
-                agent: false
-              },
-              function(error, response, request) {
-                if (!error) {
-                  const firebaseToken = body;
-                  console.log(firebaseToken);
-                  fb.auth
-                    .signInWithCustomToken(firebaseToken)
-                    .then(user => {
-                      console.log(user.user);
-                      this.$store.commit("setCurrentUser", user.user);
-                      this.$store.dispatch("fetchUserProfile").then(() => {
-                        this.$router.push("/");
-                        this.$store.commit("toggleLoading");
-                      });
-                    })
-                    .catch(err => {
-                      console.log(err);
-                      this.$store.commit("toggleLoading");
-                    });
+                function(error, response, body) {
+                    if (!error) {
+                        const userID = body;
+                        process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
+                        request.post(
+                            {
+                                url:
+                                    "https://nusocial-bridge-api.herokuapp.com/firebaseToken?id=" +
+                                    userID,
+                                headers: {
+                                    "Content-Type":
+                                        "application/x-www-form-urlencoded",
+                                },
+                                rejectUnauthorized: false,
+                                requestCert: false,
+                                agent: false,
+                            },
+                            function(error, response, request) {
+                                if (!error) {
+                                    response;
+                                    request;
+                                    const firebaseToken = body;
+                                    console.log(firebaseToken);
+                                    fb.auth
+                                        .signInWithCustomToken(firebaseToken)
+                                        .then((user) => {
+                                            console.log(user.user);
+                                            this.$store.commit(
+                                                "setCurrentUser",
+                                                user.user
+                                            );
+                                            this.$store
+                                                .dispatch("fetchUserProfile")
+                                                .then(() => {
+                                                    this.$router.push("/");
+                                                    this.$store.commit(
+                                                        "toggleLoading"
+                                                    );
+                                                });
+                                        })
+                                        .catch((err) => {
+                                            console.log(err);
+                                            this.$store.commit("toggleLoading");
+                                        });
+                                }
+                            }
+                        );
+                    }
                 }
-              }
             );
-          }
         }
-      );
     }
-  }
 );
 </script>
 
